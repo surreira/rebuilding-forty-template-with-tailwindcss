5a860810f1c43de1a82e2c1ca3ad8b1757375da4
Abel Surreira <surreira@gmail.com>
2021-04-02 19:13:55 +0100

    feat: remove boilerplate code and add header nav menu

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="box-border">
       <Head>
         <title>TailwindCSS & Next.js Forty by HTML5 UP</title>
         <link rel="icon" href="/favicon.ico" />
@@ -13,75 +13,48 @@ export default function Home() {
         />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+      <header className="relative z-10 flex items-center justify-between text-xs font-bold text-white uppercase h-11 md:h-12 xl:h-14 2xl:h-16 xl:text-sm 2xl:text-lg tracking-forty-5">
         <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="/"
+          className="inline-block px-3 font-semibold leading-5 md:px-5 2xl:px-6 group xl:leading-6 2xl:leading-8"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
+          <strong className="box-border inline-block pl-1 pr-px mr-1 transition duration-200 ease-in-out bg-white xl:mr-2 xl:pl-2 xl:pr-1 text-lucky-point group-hover:bg-anakiwa">
+            FORTY
+          </strong>
+          &nbsp;
+          <span className="hidden transition duration-200 ease-in-out group-hover:text-anakiwa sm:inline">
+            by HTML5 UP
+          </span>
         </a>
-      </footer>
+        <nav className="pl-2 pr-4 md:pl-4 md:pr-6">
+          <a
+            href="#menu"
+            className="flex items-center no-underline transition duration-200 ease-in-out hover:text-anakiwa"
+          >
+            <span className="hidden mr-1 sm:inline">Menu</span>
+            <svg
+              className="w-5"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </a>
+        </nav>
+      </header>
+
+
+
+            </p>
+
+      </main>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,25 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  purge: {
+    enable: false,
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
+      colors: {
+        anakiwa: "#9bf1ff",
+        "lucky-point": "#2a2f4a",
+      },
       fontFamily: {
         sans: ["Source Sans Pro", ...defaultTheme.fontFamily.sans],
+      },
+      letterSpacing: {
+        "forty-5": "5px",
       },
     },
   },
